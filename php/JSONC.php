0030e59e7bce547d417b383d88dd2687d3ea7ff8
unional <homawong@gmail.com>
2021-02-21 16:18:22 -0800

    fix: remove extra echo

--- php/JSONC.php
+++ php/JSONC.php
@@ -10,7 +10,6 @@ final class JSONC
   final public static function decode($json, $assoc = false, $depth = 512, $options = 0)
   {
     $trimmed = self::removeComments($json);
-    echo "trimmed: " . $trimmed;
     return json_decode($trimmed, $assoc, $depth, $options);
   }
   private static function removeComments($json)
